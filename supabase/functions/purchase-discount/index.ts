d743f270267bd7feb2da0e85b1c95d1612b753d4
WeManga <contact.wemanga@gmail.com>
2026-07-12 18:58:29 +0200

    Format code and remove unnecessary whitespace

--- supabase/functions/purchase-discount/index.ts
+++ supabase/functions/purchase-discount/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
+ 
 function generateDiscountCode() {
   const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
   let code = "RDX-";
